9d562708854b1909303387986783125742ff22ea
DaifMX <applediegoaif@gmail.com>
2025-05-25 02:46:15 -0600

    Naming convetion change to product images.

--- src/config/cors_config.ts
+++ src/config/cors_config.ts
@@ -2,7 +2,8 @@ import RuntimeError from "../errors/RuntimeError";
 
 const allowedOrigins = [
     "http://localhost:3030",
-    "http://26.232.115.193:3030" 
+    "http://26.232.115.193:3030",
+    "http://100.97.198.82:3030"
 ];
 
 const corsConfig = {
