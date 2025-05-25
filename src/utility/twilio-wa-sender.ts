ae2da2d987b3d5615a8bb74864182d9fba7bc1b0
DaifMX <applediegoaif@gmail.com>
2025-05-25 00:31:42 -0600

    Twilio sender takes phoneNumber (only mexican phones)

--- src/utility/twilio-wa-sender.ts
+++ src/utility/twilio-wa-sender.ts
@@ -1,4 +1,4 @@
-const twilioSend = (message: string) => {
+const twilioSend = (message: string, phoneNumber: bigint) => {
     const accountSid = process.env.TWILIO_ACCOUNT_SID;
     const authToken = process.env.TWILIO_AUTH_TOKEN;
     const client = require('twilio')(accountSid, authToken);
@@ -7,7 +7,7 @@ const twilioSend = (message: string) => {
         .create({
             body: message,
             from: 'whatsapp:+14155238886',
-            to: 'whatsapp:+5213324945924'
+            to: `whatsapp:+521${phoneNumber}`
         })
         .then((message: any) => message.sid);
 };
